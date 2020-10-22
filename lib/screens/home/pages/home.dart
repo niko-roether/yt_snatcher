53f1786d678013a03b17e2353c47edfe401e0818
nicholas-roether <nicholas.roether@t-online.de>
2020-10-23 00:52:06 +0200

    Add stream testing to home page

--- lib/screens/home/pages/home.dart
+++ lib/screens/home/pages/home.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:yt_snatcher/services/youtube.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomeState();
+  }
+}
+
+class HomeState extends State<Home> {
+  final YTConnection yt = YTConnection();
+  Stream<List<int>> stream;
+  String content = "";
+
+  HomeState() {
+    stream = yt.getVideoStream(
+      "fIJwDg_EP2Y",
+      YTVideoQuality(Quality.HIGHEST_QUALITY),
+    )..listen((event) {
+        this.setState(() => content += event.join(", "));
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Home"));
+    if (content == null) {
+      stream.join(", ").then((s) => setState(() => content = s));
+      return Center(child: CircularProgressIndicator());
+    }
+    return Center(child: Text(content));
   }
 }
