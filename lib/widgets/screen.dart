107d45d01767909faa854e00473be27f07df5c7f
nicholas-roether <nicholas.roether@t-online.de>
2020-10-22 18:23:13 +0200

    Create Screen widget

---
+++ lib/widgets/screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_conditional_rendering/conditional.dart';
+
+class Screen extends StatelessWidget {
+  final Widget title;
+  final Widget content;
+  final bool showSettings;
+  final Widget navigationBar;
+
+  Screen({
+    @required this.title,
+    @required this.content,
+    this.navigationBar,
+    this.showSettings = true,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: this.title, actions: [
+          Conditional.single(
+            context: context,
+            conditionBuilder: (context) => this.showSettings,
+            widgetBuilder: (context) => (IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () => Navigator.pushNamed(context, "/settings"),
+            )),
+            fallbackBuilder: (context) => Container(),
+          ),
+        ]),
+        body: content,
+        bottomNavigationBar: navigationBar);
+  }
+}
