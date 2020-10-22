22a5a72de753388f38f4667c2b2d1290fa7c257a
nicholas-roether <nicholas.roether@t-online.de>
2020-10-22 16:56:16 +0200

    Create Material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,20 @@
+import "package:flutter/material.dart";
 
+void main() => runApp(YtSnatcher());
+
+class YtSnatcher extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "YT Snatcher",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Youtube Snatcher"),
+        ),
+        body: Center(
+          child: Text("stuff and things"),
+        ),
+      ),
+    );
+  }
+}
