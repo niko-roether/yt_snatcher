9566000edf864e480a3f804ac3a9e8f3cf5154b4
Ph4ntom3 <nicholas.roether@t-online.de>
2020-10-23 19:32:14 +0200

    Create download screen

---
+++ lib/screens/downloads/download_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class DownloadScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
