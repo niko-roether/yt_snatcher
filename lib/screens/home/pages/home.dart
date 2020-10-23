8708e0b24658920c932a626e349df373fd994807
Ph4ntom3 <nicholas.roether@t-online.de>
2020-10-23 17:48:49 +0200

    render YtPlayer

--- lib/screens/home/pages/home.dart
+++ lib/screens/home/pages/home.dart
@@ -1,33 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:yt_snatcher/services/youtube.dart';
+import '../../../widgets/yt-player.dart';
+// import 'package:yt_snatcher/services/youtube.dart';
 
-class Home extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return HomeState();
-  }
-}
-
-class HomeState extends State<Home> {
-  final YTConnection yt = YTConnection();
-  Stream<List<int>> stream;
-  String content = "";
-
-  HomeState() {
-    stream = yt.getVideoStream(
-      "fIJwDg_EP2Y",
-      YTVideoQuality(Quality.HIGHEST_QUALITY),
-    )..listen((event) {
-        this.setState(() => content += "*");
-      });
-  }
-
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    if (content == null) {
-      stream.join(", ").then((s) => setState(() => content = s));
-      return Center(child: CircularProgressIndicator());
-    }
-    return Center(child: Text(content));
+    return YtPlayer(videoId: "fIJwDg_EP2Y");
   }
 }
+
+// class HomeState extends State<Home> {
+//   final YTConnection yt = YTConnection();
+//   Stream<List<int>> stream;
+//   String content = "";
+
+//   HomeState() {
+//     stream = yt.getVideoStream(
+//       "fIJwDg_EP2Y",
+//       YTVideoQuality(Quality.HIGHEST_QUALITY),
+//     )..listen((event) {
+//         this.setState(() => content += "*");
+//       });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {<<<
+//     if (content == null) {
+//       stream.join(", ").then((s) => setState(() => content = s));
+//       return Center(child: CircularProgressIndicator());
+//     }
+//     return Center(child: Text(content));
+//   }
+// }
