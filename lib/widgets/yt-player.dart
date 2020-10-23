40c80e579afb0dd82a915e5105ff4567cb16cf5d
Ph4ntom3 <nicholas.roether@t-online.de>
2020-10-23 17:48:41 +0200

    Adapt to BetterPlayer

--- lib/widgets/yt-player.dart
+++ lib/widgets/yt-player.dart
@@ -1,9 +1,9 @@
 // FIXME this doesn't work, but I decided to keep it nonetheless so that I don't forget how the VideoPlayer works
 
+import 'package:better_player/better_player.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_conditional_rendering/conditional.dart';
-import 'package:video_player/video_player.dart';
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
 class YtPlayer extends StatefulWidget {
@@ -20,7 +20,6 @@ class YtPlayer extends StatefulWidget {
 class YtPlayerState extends State<YtPlayer> {
   final YoutubeExplode _yt = YoutubeExplode();
   final String id;
-  VideoPlayerController _controller;
   StreamManifest _manifest;
 
   YtPlayerState({@required this.id}) {
@@ -41,26 +40,9 @@ class YtPlayerState extends State<YtPlayer> {
   @override
   Widget build(BuildContext context) {
     if (_manifest == null) return Center(child: CircularProgressIndicator());
-    if (_controller == null) {
-      final stream = _manifest.muxed.sortByVideoQuality().last;
-      _controller = VideoPlayerController.network(
-        stream.url.toString(),
-      )..initialize().then((_) => setState(() {}));
-    }
-    return Wrap(children: [
-      Center(
-        child: Conditional.single(
-          context: context,
-          conditionBuilder: (_) => _controller.value.initialized,
-          widgetBuilder: (context) => AspectRatio(
-            aspectRatio: _controller.value.aspectRatio,
-            child: VideoPlayer(_controller),
-          ),
-          fallbackBuilder: (context) => Center(
-            child: CircularProgressIndicator(),
-          ),
-        ),
-      ),
-    ]);
+    var stream = _manifest.muxed.sortByVideoQuality().last;
+    return Center(
+      child: BetterPlayer.network(stream.url.toString()),
+    );
   }
 }
