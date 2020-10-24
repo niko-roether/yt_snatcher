01df44f1595daf33fde01cc477aecd61bba18e1e
Ph4ntom3 <nicholas.roether@t-online.de>
2020-10-24 19:49:56 +0200

    Add Dimension and listSort utility

--- lib/util.dart
+++ lib/util.dart
@@ -1,3 +1,28 @@
 Stream<T> futureIntoStream<T>(Future<Stream<T>> streamFuture) {
   return Stream.fromFuture(streamFuture).asyncExpand((event) => event);
 }
+
+class Dimension extends Comparable<Dimension> {
+  int width;
+  int height;
+
+  Dimension(this.width, this.height);
+
+  int size() => width * height;
+
+  @override
+  String toString() {
+    return "$width x $height";
+  }
+
+  @override
+  int compareTo(Dimension other) {
+    return size() - other.size();
+  }
+}
+
+// Returns a sorted copy of a list instead of sorting the list itself
+List<T> listSort<T>(List<T> list, int Function(T, T) compare) {
+  list.sort(compare);
+  return list;
+}
