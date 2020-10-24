1546ae619151b21105bbb72b26a6864a6f9098d0
Ph4ntom3 <nicholas.roether@t-online.de>
2020-10-24 19:49:06 +0200

    Implement new youtube api

--- lib/services/youtube.dart
+++ lib/services/youtube.dart
@@ -1,73 +1,186 @@
-import 'dart:async';
+// yt.getVideo("dfsdghdfg").muxed.highestQuality();
+
+import 'package:flutter/material.dart';
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
+import 'package:yt_snatcher/util.dart';
 
-// Youtube.getManifest("dsfadsf").stream(info);
+class YoutubeThumbnailSet {
+  final ThumbnailSet _thumbnailSet;
 
-class YoutubeStream<I extends StreamInfo> {
+  YoutubeThumbnailSet(this._thumbnailSet);
+
+  String get lowRes => _thumbnailSet.lowResUrl;
+  String get mediumRes => _thumbnailSet.mediumResUrl;
+  String get highRes => _thumbnailSet.highResUrl;
+  String get maxRes => _thumbnailSet.maxResUrl;
+  String get standartRes => _thumbnailSet.standardResUrl;
+}
+
+class YoutubeMedia<I extends StreamInfo> {
   final YoutubeExplode _yt;
-  final I info;
-  YoutubeStream(this.info, this._yt);
+  final I _info;
 
-  Stream<List<int>> getDataStream() {
-    return _yt.videos.streamsClient.get(info);
+  YoutubeMedia(this._info, this._yt);
+
+  int get tag => _info.tag;
+  String get url => _info.url.path;
+  String get container => _info.container.name;
+  int get size => _info.size.totalBytes;
+  int get bitrate => _info.bitrate.bitsPerSecond;
+
+  Stream<List<int>> getStream() => _yt.videos.streamsClient.get(_info);
+}
+
+class YoutubeAudioMedia extends YoutubeMedia<AudioStreamInfo> {
+  YoutubeAudioMedia(AudioStreamInfo info, YoutubeExplode yt) : super(info, yt);
+
+  String get audioCodec => _info.audioCodec;
+}
+
+class YoutubeVideoMedia extends YoutubeMedia<VideoStreamInfo> {
+  YoutubeVideoMedia(VideoStreamInfo info, YoutubeExplode yt) : super(info, yt);
+
+  String get videoCodec => _info.videoCodec;
+  String get videoQuality => _info.videoQualityLabel;
+  Dimension get resolution =>
+      Dimension(_info.videoResolution.width, _info.videoResolution.height);
+  num get framerate => _info.framerate.framesPerSecond;
+}
+
+class YoutubeMuxedMedia extends YoutubeMedia<MuxedStreamInfo> {
+  YoutubeMuxedMedia(MuxedStreamInfo info, YoutubeExplode yt) : super(info, yt);
+
+  // FIXME code repetition here.
+  String get videoCodec => _info.videoCodec;
+  String get videoQualityLabel => _info.videoQualityLabel;
+  Dimension get resolution =>
+      Dimension(_info.videoResolution.width, _info.videoResolution.height);
+  num get framerate => _info.framerate.framesPerSecond;
+  String get audioCodec => _info.audioCodec;
+}
+
+class YoutubeMediaSet<M extends YoutubeMedia> extends Iterable<M> {
+  static final HIGHEST_BITRATE =
+      (YoutubeMedia a, YoutubeMedia b) => a.bitrate.compareTo(b.bitrate);
+  static final LARGEST_SIZE =
+      (YoutubeMedia a, YoutubeMedia b) => a.size.compareTo(b.size);
+  final List<M> _media;
+
+  YoutubeMediaSet(this._media);
+
+  void sortByBitrate() => _media.sort((a, b) => a.bitrate.compareTo(b.bitrate));
+
+  M highestBitrate() {
+    return listSort(_media, HIGHEST_BITRATE).last;
   }
 
-  void close() {
-    _yt.close();
+  M smallestSize() {
+    return listSort(_media, LARGEST_SIZE).first;
+  }
+
+  @override
+  Iterator<M> get iterator => _media.iterator;
+}
+
+class YoutubeVideoMediaSet extends YoutubeMediaSet<YoutubeVideoMedia> {
+  static final HIGHEST_RESOLUTION = (YoutubeVideoMedia a, YoutubeVideoMedia b) {
+    return a.resolution.compareTo(b.resolution);
+  };
+  YoutubeVideoMediaSet(List<YoutubeVideoMedia> media) : super(media);
+
+  YoutubeVideoMedia highestResolution() {
+    return listSort(_media, HIGHEST_RESOLUTION).last;
   }
 }
 
-class YoutubeStreamSet<I extends StreamInfo> {
-  final List<YoutubeStream<I>> streams;
-  YoutubeStreamSet(this.streams);
+class YoutubeAudioMediaSet extends YoutubeMediaSet<YoutubeAudioMedia> {
+  YoutubeAudioMediaSet(List<YoutubeAudioMedia> media) : super(media);
 
-  factory YoutubeStreamSet.from(Iterable<I> streams, YoutubeExplode yt) {
-    return YoutubeStreamSet(streams.map((e) => YoutubeStream(e, yt)));
+  // Additional sorting options if they present themselves in the future
+}
+
+class YoutubeMuxedMediaSet extends YoutubeMediaSet<YoutubeMuxedMedia> {
+  static final HIGHEST_RESOLUTION = (YoutubeMuxedMedia a, YoutubeMuxedMedia b) {
+    return a.resolution.compareTo(b.resolution);
+  };
+  YoutubeMuxedMediaSet(List<YoutubeMuxedMedia> media) : super(media);
+
+  // FIXME Code repetition again...
+  YoutubeMuxedMedia highestResolution() {
+    return listSort(_media, HIGHEST_RESOLUTION).last;
   }
 }
 
-class YoutubeManifest {
+class YoutubeVideoMeta {
+  final Video _video;
+
+  YoutubeVideoMeta(this._video);
+
+  String get title => _video.title;
+  String get youtubeUrl => _video.url;
+  String get description => _video.description;
+  String get channelName => _video.author;
+  String get channelId => _video.channelId.value;
+  Duration get duration => _video.duration;
+  YoutubeThumbnailSet get thumbnails => YoutubeThumbnailSet(_video.thumbnails);
+  DateTime get uploadDate => _video.uploadDate;
+}
+
+class YoutubeVideo extends YoutubeVideoMeta {
   final YoutubeExplode _yt;
-  final StreamManifest manifest;
-  YoutubeManifest(this.manifest, this._yt);
+  final StreamManifest _manifest;
 
-  YoutubeStreamSet<AudioStreamInfo> get audio {
-    return YoutubeStreamSet.from(manifest.audio, _yt);
+  YoutubeVideo(Video video, this._manifest, this._yt) : super(video);
+
+  // Streams
+  YoutubeMediaSet get streams {
+    return YoutubeMediaSet(_manifest.streams.map((e) => YoutubeMedia(e, _yt)));
   }
 
-  YoutubeStreamSet<AudioOnlyStreamInfo> get audioOnly {
-    return YoutubeStreamSet.from(manifest.audioOnly, _yt);
+  YoutubeVideoMediaSet withVideo() {
+    return YoutubeVideoMediaSet(
+      _manifest.video.map((e) => YoutubeVideoMedia(e, _yt)),
+    );
   }
 
-  YoutubeStreamSet<MuxedStreamInfo> get muxed {
-    return YoutubeStreamSet.from(manifest.muxed, _yt);
+  YoutubeVideoMediaSet videoOnly() {
+    return YoutubeVideoMediaSet(
+      _manifest.videoOnly.map((e) => YoutubeVideoMedia(e, _yt)),
+    );
   }
 
-  YoutubeStreamSet<VideoStreamInfo> get video {
-    return YoutubeStreamSet.from(manifest.video, _yt);
+  YoutubeAudioMediaSet withAudio() {
+    return YoutubeAudioMediaSet(
+      _manifest.audio.map((e) => YoutubeAudioMedia(e, _yt)),
+    );
   }
 
-  YoutubeStreamSet<VideoOnlyStreamInfo> get videoOnly {
-    return YoutubeStreamSet.from(manifest.videoOnly, _yt);
+  YoutubeAudioMediaSet audioOnly() {
+    return YoutubeAudioMediaSet(
+      _manifest.audio.map((e) => YoutubeAudioMedia(e, _yt)),
+    );
   }
 
-  YoutubeStreamSet<StreamInfo> get all {
-    return YoutubeStreamSet.from(manifest.streams, _yt);
+  YoutubeMuxedMediaSet muxed() {
+    return YoutubeMuxedMediaSet(
+      _manifest.muxed.map((e) => YoutubeMuxedMedia(e, _yt)),
+    );
   }
 }
 
 class Youtube {
-  final _yt = YoutubeExplode();
+  final YoutubeExplode _yt = YoutubeExplode();
 
-  Future<YoutubeManifest> getManifest(String videoId) async {
-    return YoutubeManifest(
-      await _yt.videos.streamsClient.getManifest(videoId),
-      _yt,
-    );
+  Future<YoutubeVideoMeta> getVideoMeta(String id) async {
+    return YoutubeVideoMeta(await _yt.videos.get(id));
   }
 
-  Future<String> getVideoURL(String videoId) {
-    return _yt.videos.streamsClient.getHttpLiveStreamUrl(VideoId(videoId));
+  Future<YoutubeVideo> getVideo(String id) async {
+    return YoutubeVideo(
+      await _yt.videos.get(id),
+      await _yt.videos.streamsClient.getManifest(id),
+      _yt,
+    );
   }
 
   void close() {
@@ -75,81 +188,94 @@ class Youtube {
   }
 }
 
+// --- Previous Implementation ---
+
+// import 'dart:async';
 // import 'package:youtube_explode_dart/youtube_explode_dart.dart';
-// import 'package:yt_snatcher/util.dart';
 
-// class Quality {
-//   static const HIGHEST_BITRATE = "highest_bitrate";
-//   static const LOWEST_BITRATE = "lowest_bitrate";
-//   static const HIGHEST_QUALITY = "highest_quality";
-//   static const LOWEST_QUALITY = "lowest_quality";
+// // Youtube.getManifest("dsfadsf").stream(info);
+
+// class YoutubeMedia<I extends StreamInfo> {
+//   final YoutubeExplode _yt;
+//   final I _info;
+//   YoutubeMedia(this._info, this._yt);
+
+//   Stream<List<int>> getDataStream() {
+//     return _yt.videos.streamsClient.get(_info);
+//   }
+
+//   String get url {
+//     return _info.url.path;
+//   }
+
+//   void close() {
+//     _yt.close();
+//   }
 // }
 
-// abstract class YTQuality<T extends StreamInfo> {
-//   final String quality;
+// class YoutubeMediaSet<I extends StreamInfo> {
+//   final YoutubeExplode _yt;
+//   final Iterable<I> _streams;
+//   YoutubeMediaSet(this._streams, this._yt);
 
-//   YTQuality(this.quality);
+//   YoutubeMedia<RI> _getYoutubeMedia<RI extends StreamInfo>(RI info) {
+//     return YoutubeMedia<RI>(info, _yt);
+//   }
 
-//   T getStreamInfo(Iterable<T> options) {
-//     switch (quality) {
-//       case Quality.HIGHEST_BITRATE:
-//         return options.withHighestBitrate();
-//       case Quality.LOWEST_BITRATE:
-//         return options.sortByBitrate().first;
-//       default:
-//         throw "Invalid quality string '$quality' ";
+//   List<YoutubeMedia<I>> get streams => _streams.map((e) => _getYoutubeMedia(e));
+
+//   YoutubeMedia<I> get highestBitrate {
+//     return _getYoutubeMedia(_streams.withHighestBitrate());
+//   }
+
+//   YoutubeMedia<VideoStreamInfo> get highestQuality {
+//     if (I is VideoStreamInfo) {
+//       return _getYoutubeMedia(
+//         (_streams as List<VideoStreamInfo>).sortByVideoQuality().last,
+//       );
 //     }
+//     throw "This media set does not contain any video streams";
 //   }
 // }
 
-// class YTVideoQuality extends YTQuality<MuxedStreamInfo> {
-//   YTVideoQuality(String quality) : super(quality);
+// class YoutubeManifest {
+//   final YoutubeExplode _yt;
+//   final StreamManifest manifest;
+//   YoutubeManifest(this.manifest, this._yt);
 
-//   @override
-//   MuxedStreamInfo getStreamInfo(Iterable<MuxedStreamInfo> options) {
-//     if (options.getAllVideoQualitiesLabel().contains(options))
-//       return options
-//           .firstWhere((element) => element.videoQualityLabel == quality);
-//     switch (quality) {
-//       case Quality.HIGHEST_QUALITY:
-//         return options.sortByVideoQuality().last;
-//       case Quality.LOWEST_QUALITY:
-//         return options.sortByVideoQuality().first;
-//       default:
-//         return super.getStreamInfo(options);
-//     }
+//   YoutubeMediaSet<AudioStreamInfo> get audio {
+//     return YoutubeMediaSet(manifest.audio, _yt);
+//   }
+
+//   YoutubeMediaSet<AudioOnlyStreamInfo> get audioOnly {
+//     return YoutubeMediaSet(manifest.audioOnly, _yt);
+//   }
+
+//   YoutubeMediaSet<MuxedStreamInfo> get muxed {
+//     return YoutubeMediaSet(manifest.muxed, _yt);
+//   }
+
+//   YoutubeMediaSet<VideoStreamInfo> get video {
+//     return YoutubeMediaSet(manifest.video, _yt);
+//   }
+
+//   YoutubeMediaSet<VideoOnlyStreamInfo> get videoOnly {
+//     return YoutubeMediaSet(manifest.videoOnly, _yt);
+//   }
+
+//   YoutubeMediaSet<StreamInfo> get all {
+//     return YoutubeMediaSet(manifest.streams, _yt);
 //   }
 // }
 
-// class YTConnection {
-//   YoutubeExplode _yt;
+// class Youtube {
+//   final _yt = YoutubeExplode();
 
-//   YTConnection() {
-//     _yt = YoutubeExplode();
-//   }
-
-//   Future<StreamManifest> _getManifest(String id) {
-//     return _yt.videos.streamsClient.getManifest(id);
-//   }
-
-//   Stream<List<int>> _getStreamFromInfo(StreamInfo streamInfo) {
-//     return _yt.videos.streamsClient.get(streamInfo);
-//   }
-
-//   Stream<List<int>> getVideoStream(String id, YTQuality quality) {
-//     var streamFuture = _getManifest(id).then((manifest) {
-//       var info = quality.getStreamInfo(manifest.muxed);
-//       return _getStreamFromInfo(info);
-//     });
-//     return futureIntoStream(streamFuture);
-//   }
-
-//   Stream<List<int>> getAudioStream(String id, YTQuality quality) {
-//     var streamFuture = _getManifest(id).then((manifest) {
-//       var info = quality.getStreamInfo(manifest.audioOnly);
-//       return _getStreamFromInfo(info);
-//     });
-//     return futureIntoStream(streamFuture);
+//   Future<YoutubeManifest> getManifest(String videoId) async {
+//     return YoutubeManifest(
+//       await _yt.videos.streamsClient.getManifest(videoId),
+//       _yt,
+//     );
 //   }
 
 //   void close() {
