515190342cfc3ca2a219a31768ef67e3314b7ccd
nicholas-roether <nicholas.roether@t-online.de>
2020-10-25 02:07:07 +0200

    Create YTVideoInfo widget

---
+++ lib/widgets/yt-video-info.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:yt_snatcher/services/youtube.dart';
+
+class YTVideoInfo extends StatelessWidget {
+  final YoutubeVideoMeta videoMeta;
+
+  YTVideoInfo(this.videoMeta);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
