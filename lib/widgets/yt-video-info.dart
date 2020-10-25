d6601eac34f4977f8776a96dece2efb234de126b
nicholas-roether <nicholas.roether@t-online.de>
2020-10-25 14:12:47 +0100

    Implement YTVideoInfo component

--- lib/widgets/yt-video-info.dart
+++ lib/widgets/yt-video-info.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:yt_snatcher/services/youtube.dart';
 
 class YTVideoInfo extends StatelessWidget {
@@ -6,9 +6,36 @@ class YTVideoInfo extends StatelessWidget {
 
   YTVideoInfo(this.videoMeta);
 
+  Map<String, String> get infoMap => {
+        "Title": videoMeta.title,
+        "Description": videoMeta.description,
+        "Upload Date": videoMeta.uploadDate.toString(),
+        "Duration": videoMeta.duration.toString(),
+        "Youtube-URL": videoMeta.youtubeUrl,
+        "Channel Name": videoMeta.channelName,
+        "Channel ID": videoMeta.channelId
+      };
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    var getTableCell = (String text) => TableCell(
+          child: Container(
+            child: Text(text),
+            padding: EdgeInsets.all(5),
+          ),
+        );
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      child: Table(
+        children: infoMap.entries
+            .map(
+              (e) => TableRow(
+                key: ValueKey(e.key),
+                children: [getTableCell(e.key), getTableCell(e.value)],
+              ),
+            )
+            .toList(),
+      ),
+    );
   }
 }
