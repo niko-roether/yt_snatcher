367221378958339242465d44204e5e5a8faf5aa3
nicholas-roether <nicholas.roether@t-online.de>
2020-10-25 14:13:21 +0100

    Use routenames defined by screen widgets

--- lib/router.dart
+++ lib/router.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:yt_snatcher/screens/downloads/download_screen.dart';
 import 'package:yt_snatcher/screens/home/home_screen.dart';
+import 'package:yt_snatcher/screens/video_info/video_info_screen.dart';
 import 'package:yt_snatcher/widgets/screen.dart';
 
 var routes = {
-  "/": (context) => HomeScreen(),
+  HomeScreen.ROUTENAME: (context) => HomeScreen(),
+  DownloadScreen.ROUTENAME: (context) => DownloadScreen(),
+  VideoInfoScreen.ROUTENAME: (context) => VideoInfoScreen(),
   "/settings": (context) => Screen(
         title: Text("Settings"),
         content: Text("Settings"),
         showSettings: false,
-      )
+      ),
 };
