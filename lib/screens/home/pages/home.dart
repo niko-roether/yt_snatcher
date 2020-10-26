3eba8b4cdd8392bd92f01611066e00ff75573017
Ph4ntom3 <nicholas.roether@t-online.de>
2020-10-27 00:34:05 +0100

    Create temporary debug page

--- lib/screens/home/pages/home.dart
+++ lib/screens/home/pages/home.dart
@@ -1,9 +1,36 @@
+import 'package:better_player/better_player.dart';
 import 'package:flutter/material.dart';
-import '../../../widgets/yt-player.dart';
+import 'package:yt_snatcher/services/download.dart';
+import 'package:yt_snatcher/services/youtube-dl.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomeState();
+  }
+}
+
+class HomeState extends State<Home> {
+  final _dl = DownloadManager();
+  final _ytdl = YoutubeDL();
+  List<Download> _downloads;
+
+  HomeState() {
+    _init();
+  }
+
+  void _init() async {
+    var preDl = await _ytdl.prepare("fad_0eQIlVo").asVideo();
+    var dl = await preDl.best().download((pr) => print(pr));
+    print(dl.meta.filename);
+    var videos = await _dl.getVideos();
+    var dls = await videos.getDownloads();
+    setState(() => _downloads = dls);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return YtPlayer(videoId: "zUpOXtxFLWw");
+    if (_downloads == null) return Center(child: CircularProgressIndicator());
+    return BetterPlayer.file(_downloads[0].mediaFile.path);
   }
 }
