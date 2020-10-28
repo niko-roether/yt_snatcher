afaf80dad19a11cb51badb6f0547a54bafec9758
Ph4ntom3 <nicholas.roether@t-online.de>
2020-10-28 13:18:01 +0100

    Create DownloaderView widget

---
+++ lib/widgets/downloader_view.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:yt_snatcher/services/youtube-dl.dart';
+import 'package:yt_snatcher/services/youtube.dart';
+import 'package:yt_snatcher/widgets/download_progress_indicator.dart';
+
+class DownloaderView extends StatefulWidget {
+  final Downloader downloader;
+  final VideoMeta meta;
+
+  DownloaderView({@required this.downloader, @required this.meta});
+
+  @override
+  State<StatefulWidget> createState() {
+    return DownloaderViewState(downloader, meta);
+  }
+}
+
+class DownloaderViewState extends State<DownloaderView> {
+  final Downloader _downloader;
+  final VideoMeta _meta;
+  double _progress = 0;
+  String _stage = "Preparing";
+
+  DownloaderViewState(this._downloader, this._meta) {
+    _downloader.progressStream.listen(
+      (event) => setState(() {
+        _progress = event.progress;
+        _stage = event.stage;
+      }),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DownloadProgressIndicator(
+      title: _meta.title,
+      subtitle: _meta.channelName,
+      progress: _progress,
+      stage: _stage,
+      thumbnailUrl: _meta.thumbnails.lowRes,
+      semanticName: _meta.title,
+    );
+  }
+}
