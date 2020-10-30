77f04186591eec7c29cd4885b0d1615d4dd5cb10
Ph4ntom3 <nicholas.roether@t-online.de>
2020-10-30 19:05:19 +0100

    Rename download to download_manager; Remove functionality not related to managing existing downloads

--- lib/services/download_magager.dart
+++ lib/services/download_magager.dart
@@ -2,9 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
 
-import 'package:yt_snatcher/services/background.dart';
 import 'package:yt_snatcher/services/files.dart' as fs;
-import 'package:yt_snatcher/services/muxer.dart' as mx;
 import 'package:yt_snatcher/services/youtube.dart' as yt;
 
 enum DownloadType { VIDEO, MUSIC }
@@ -145,169 +143,10 @@ class MusicDownloadSet extends DownloadSet {
 }
 
 class DownloadManager {
-  static const _NUM_DOWNLOAD_THREADS = 3;
   final _fileManager = fs.FileManager();
-  final _muxer = mx.Muxer();
-  final _threadPool = ThreadPool(_NUM_DOWNLOAD_THREADS);
 
   static String getFilename(String name, yt.Media media) =>
       "$name.${media.container}";
-
-  static Future<Download> _createDownload(
-    List<Future<File>> fileFutures,
-  ) async {
-    var files = await Future.wait(fileFutures);
-    var meta = DownloadMeta.fromJson(await files[0].readAsString(), files[0]);
-    return Download(meta, files[1]);
-  }
-
-  Stream<List<int>> _monitoredStream(
-    Stream<List<int>> stream,
-    void Function(int) onProgress,
-  ) {
-    return stream.asyncMap((packet) {
-      onProgress?.call(packet.length);
-      return packet;
-    });
-  }
-
-  String _metaFileName(String name) => "$name.json";
-  String _muxedFileName(String name) => "$name.mp4";
-
-  Future<Download> _download(
-    Future<File> metaFileFuture,
-    Future<File> mediaFileFuture, {
-    int load,
-  }) {
-    return _threadPool.run(
-      _createDownload,
-      [metaFileFuture, mediaFileFuture],
-      load: load,
-    );
-  }
-
-  Future<File> _downloadMusicMedia(
-    String filename,
-    yt.AudioMedia media, [
-    void Function(int) onProgress,
-  ]) {
-    var audioStream = _monitoredStream(media.getStream(), onProgress);
-    return _fileManager.streamLocalFile(
-        fs.FileManager.MUSIC_PATH, filename, audioStream);
-  }
-
-  Future<File> _downloadVideoMedia(
-    String filename,
-    yt.VideoMedia video,
-    yt.AudioMedia audio, [
-    void Function(int, String) onProgress,
-  ]) async {
-    var videoStream = _monitoredStream(
-      video.getStream(),
-      (p) => onProgress(p, "Loading"),
-    );
-    var videoFileFuture = _fileManager.streamTempFile(
-      "video_$filename.${video.container}",
-      videoStream,
-    );
-
-    var audioStream = _monitoredStream(
-      audio.getStream(),
-      (p) => onProgress(p, "Loading"),
-    );
-    var audioFileFuture = _fileManager.streamTempFile(
-      "audio_$filename.${audio.container}",
-      audioStream,
-    );
-
-    var files = await Future.wait([videoFileFuture, audioFileFuture]);
-    if (files.any((f) => f == null)) throw "Failed to get media files";
-
-    var muxedFile = await _fileManager.createLocalFile(
-      fs.FileManager.VIDEO_PATH,
-      filename,
-    );
-
-    // TODO monitor muxing progress
-    await _muxer.mux(
-      files[0].path,
-      files[1].path,
-      muxedFile.path,
-      (p) => onProgress(p, "Processing"),
-    );
-    files.forEach((f) => f.delete());
-
-    return muxedFile;
-  }
-
-  Future<File> _downloadMeta(
-    String path,
-    String id,
-    String mediaFilename,
-    yt.VideoMeta meta,
-  ) {
-    var filename = _metaFileName(id);
-    var dlMeta = DownloadMeta(
-      meta,
-      id,
-      mediaFilename,
-      null,
-      DownloadType.MUSIC,
-    );
-    return _fileManager.writeLocalFile(
-      path,
-      filename,
-      dlMeta.toJson(),
-    );
-  }
-
-  Future<File> _downloadMusicMeta(
-    String id,
-    String mediaFilename,
-    yt.VideoMeta meta,
-  ) =>
-      _downloadMeta(
-        fs.FileManager.MUSIC_META_PATH,
-        id,
-        mediaFilename,
-        meta,
-      );
-
-  Future<File> _downloadVideoMeta(
-    String id,
-    String mediaFilename,
-    yt.VideoMeta meta,
-  ) =>
-      _downloadMeta(fs.FileManager.VIDEO_META_PATH, id, mediaFilename, meta);
-
-  Future<Download> downloadMusic(
-    String name,
-    yt.VideoMeta meta,
-    yt.AudioMedia media, [
-    void Function(int) onProgress,
-  ]) {
-    var filename = "$name.${media.container}";
-    return _download(
-      _downloadMusicMeta(name, filename, meta),
-      _downloadMusicMedia(filename, media, onProgress),
-      load: 1,
-    );
-  }
-
-  Future<Download> downloadVideo(
-    String name,
-    yt.VideoMeta meta,
-    yt.VideoMedia video,
-    yt.AudioMedia audio, [
-    void Function(int, String) onProgress,
-  ]) {
-    var filename = _muxedFileName(name);
-    return _download(
-      _downloadVideoMeta(name, filename, meta),
-      _downloadVideoMedia(filename, video, audio, onProgress),
-      load: 3,
-    );
-  }
 
   Future<List<DownloadMeta>> _extractMetaData(List<File> metaFiles) {
     return Future.wait(metaFiles
@@ -334,9 +173,5 @@ class DownloadManager {
       await _getMetaData(fs.FileManager.MUSIC_META_PATH),
       _fileManager,
     );
-  }
-
-  void close() {
-    _threadPool.close();
   }
 }
