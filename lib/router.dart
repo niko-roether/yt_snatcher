b70b8979eb30cda9c07b44a3476b6b0d332b186d
nicholas-roether <nicholas.roether@t-online.de>
2020-10-31 19:45:01 +0100

    Remove obsolete route

--- lib/router.dart
+++ lib/router.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:yt_snatcher/screens/downloads/download_screen.dart';
+import 'package:yt_snatcher/screens/home/pages/downloads.dart';
 import 'package:yt_snatcher/screens/home/home_screen.dart';
 import 'package:yt_snatcher/screens/video_info/video_info_screen.dart';
 import 'package:yt_snatcher/widgets/screen.dart';
 
 var routes = {
   HomeScreen.ROUTENAME: (context) => HomeScreen(),
-  DownloadScreen.ROUTENAME: (context) => DownloadScreen(),
   VideoInfoScreen.ROUTENAME: (context) => VideoInfoScreen(),
   "/settings": (context) => Screen(
         title: Text("Settings"),
