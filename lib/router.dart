98e7815a896c72ebd1f3ddaf68a77482cd9ef339
nicholas-roether <nicholas.roether@t-online.de>
2020-10-31 20:55:00 +0100

    Add download screen to router

--- lib/router.dart
+++ lib/router.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:yt_snatcher/screens/home/pages/downloads.dart';
+import 'package:yt_snatcher/screens/download/download_screen.dart';
 import 'package:yt_snatcher/screens/home/home_screen.dart';
 import 'package:yt_snatcher/screens/video_info/video_info_screen.dart';
 import 'package:yt_snatcher/widgets/screen.dart';
@@ -7,6 +7,7 @@ import 'package:yt_snatcher/widgets/screen.dart';
 var routes = {
   HomeScreen.ROUTENAME: (context) => HomeScreen(),
   VideoInfoScreen.ROUTENAME: (context) => VideoInfoScreen(),
+  DownloadScreen.ROUTENAME: (context) => DownloadScreen(),
   "/settings": (context) => Screen(
         title: Text("Settings"),
         content: Text("Settings"),
