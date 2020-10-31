429ce85eb98edbecfb73a1c8adc13cc5c618dde9
nicholas-roether <nicholas.roether@t-online.de>
2020-10-31 19:49:57 +0100

    Create Downloads page

---
+++ lib/screens/home/pages/downloads.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:yt_snatcher/screens/home/downloads_display.dart';
+
+class Downloads extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => DownloadsDisplay();
+}
