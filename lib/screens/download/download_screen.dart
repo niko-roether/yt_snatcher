8ff7fe4d4ee111ffa5870126f4ecb89ff4404ab0
nicholas-roether <nicholas.roether@t-online.de>
2020-10-31 20:54:38 +0100

    Create download screen

---
+++ lib/screens/download/download_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class DownloadScreen extends StatelessWidget {
+  static const ROUTENAME = "/download";
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
