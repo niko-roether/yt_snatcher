b788226dc44c722ab4653f6a5277a28e9b8dfcc2
nicholas-roether <nicholas.roether@t-online.de>
2020-11-01 08:30:30 +0100

    Add add FAB

--- lib/screens/home/pages/downloads.dart
+++ lib/screens/home/pages/downloads.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:yt_snatcher/screens/download/download_screen.dart';
 import 'package:yt_snatcher/screens/home/download_processes_display.dart';
 
 class Downloads extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => DownloadProcessesDisplay();
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      Center(child: DownloadProcessesDisplay()),
+      Align(
+        child: Padding(
+          child: FloatingActionButton(
+            onPressed: () =>
+                Navigator.pushNamed(context, DownloadScreen.ROUTENAME),
+            child: Icon(Icons.add),
+          ),
+          padding: EdgeInsets.all(16),
+        ),
+        alignment: Alignment.bottomRight,
+      ),
+    ]);
+  }
 }
