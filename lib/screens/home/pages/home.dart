f6beebfb8a1de5721594ca88826117427f68693f
nicholas-roether <nicholas.roether@t-online.de>
2020-11-01 09:17:59 +0100

    Remove FAB

--- lib/screens/home/pages/home.dart
+++ lib/screens/home/pages/home.dart
@@ -5,7 +5,13 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: DownloadForm(),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: Text(
+          "Absolutely amazing home screen.\n\nBy using our app you agree to our privacy policy as well as to be absolutely blown away by how amazing our home screen is.",
+          textAlign: TextAlign.center,
+        ),
+      ),
     );
   }
 }
