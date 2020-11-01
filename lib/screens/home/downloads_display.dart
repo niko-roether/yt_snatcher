49c13ac29d8d8a53959977000ceaca22b1e36526
nicholas-roether <nicholas.roether@t-online.de>
2020-11-01 08:30:15 +0100

    Add onTap callback

--- lib/screens/home/downloads_display.dart
+++ lib/screens/home/downloads_display.dart
@@ -2,14 +2,15 @@ import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:yt_snatcher/services/download_magager.dart';
+import 'package:yt_snatcher/services/download_manager.dart';
 import 'package:yt_snatcher/widgets/download_view.dart';
 import 'package:yt_snatcher/widgets/provider/download_provider.dart';
 
 class DownloadsDisplay extends StatefulWidget {
   final FutureOr<DownloadSet> Function(DownloadManager dlm) selector;
+  final void Function(Download download) onTap;
 
-  DownloadsDisplay(this.selector);
+  DownloadsDisplay({@required this.selector, this.onTap});
 
   @override
   State<StatefulWidget> createState() {
@@ -47,7 +48,10 @@ class DownloadsDisplayState extends State<DownloadsDisplay> {
       child: ListView.builder(
         itemBuilder: (context, i) {
           var download = _downloads[i];
-          return DownloadView(download: download);
+          return DownloadView(
+            download: download,
+            onTap: () => widget.onTap?.call(download),
+          );
         },
         itemCount: _downloads.length,
       ),
