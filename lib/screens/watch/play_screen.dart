ed4435fc8192586fb97d14341e2ed3978a6a5be0
nicholas-roether <nicholas.roether@t-online.de>
2020-11-01 09:17:18 +0100

    Refactur watch_screen to play_screen

---
+++ lib/screens/watch/play_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:yt_snatcher/services/download_manager.dart';
+import 'package:yt_snatcher/widgets/download_player.dart';
+import 'package:yt_snatcher/widgets/screen.dart';
+
+class PlayScreen extends StatelessWidget {
+  static const ROUTENAME = "/play";
+
+  @override
+  Widget build(BuildContext context) {
+    Download dl = ModalRoute.of(context).settings.arguments;
+    Widget content;
+    if (dl == null) content = Center(child: Text("No video found"));
+    content = DownloadPlayer(download: dl);
+    return Screen(
+      title: Text(
+        dl.meta.type == DownloadType.VIDEO ? "Watch Video" : "Listen to Music",
+      ),
+      content: content,
+      key: Key(ROUTENAME),
+    );
+  }
+}
