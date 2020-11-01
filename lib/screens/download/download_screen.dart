b2249ab45b5b5b8cac3d816a5d4420894448f8d0
nicholas-roether <nicholas.roether@t-online.de>
2020-11-01 08:30:04 +0100

    Implement download screen

--- lib/screens/download/download_screen.dart
+++ lib/screens/download/download_screen.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/cupertino.dart';
+import 'package:yt_snatcher/services/download_manager.dart';
+import 'package:yt_snatcher/widgets/download_form.dart';
+import 'package:yt_snatcher/widgets/screen.dart';
 
 class DownloadScreen extends StatelessWidget {
   static const ROUTENAME = "/download";
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    DownloadType type = ModalRoute.of(context).settings.arguments;
+    return Screen(
+      title: Text("Download Youtube Media"),
+      key: Key(ROUTENAME),
+      content: Padding(
+        padding: EdgeInsets.only(top: 18),
+        child: DownloadForm(initialDownloadType: type ?? DownloadType.VIDEO),
+      ),
+    );
   }
 }
