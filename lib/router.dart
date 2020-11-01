1e27f4f3eba019bc150ec068848db1351ac9b6d8
nicholas-roether <nicholas.roether@t-online.de>
2020-11-01 08:29:50 +0100

    Add watch screen

--- lib/router.dart
+++ lib/router.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:yt_snatcher/screens/download/download_screen.dart';
 import 'package:yt_snatcher/screens/home/home_screen.dart';
 import 'package:yt_snatcher/screens/video_info/video_info_screen.dart';
+import 'package:yt_snatcher/screens/watch/watch_screen.dart';
 import 'package:yt_snatcher/widgets/screen.dart';
 
 var routes = {
   HomeScreen.ROUTENAME: (context) => HomeScreen(),
   VideoInfoScreen.ROUTENAME: (context) => VideoInfoScreen(),
   DownloadScreen.ROUTENAME: (context) => DownloadScreen(),
+  WatchScreen.ROUTENAME: (context) => WatchScreen(),
   "/settings": (context) => Screen(
         title: Text("Settings"),
         content: Text("Settings"),
