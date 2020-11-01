6e67b7ecbc0eefcbde65452f5d7609d9a19fd503
nicholas-roether <nicholas.roether@t-online.de>
2020-11-01 09:18:43 +0100

    Remove FAB

--- lib/screens/home/pages/downloads.dart
+++ lib/screens/home/pages/downloads.dart
@@ -1,23 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:yt_snatcher/screens/download/download_screen.dart';
 import 'package:yt_snatcher/screens/home/download_processes_display.dart';
 
 class Downloads extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return Stack(children: [
-      Center(child: DownloadProcessesDisplay()),
-      Align(
-        child: Padding(
-          child: FloatingActionButton(
-            onPressed: () =>
-                Navigator.pushNamed(context, DownloadScreen.ROUTENAME),
-            child: Icon(Icons.add),
-          ),
-          padding: EdgeInsets.all(16),
-        ),
-        alignment: Alignment.bottomRight,
-      ),
-    ]);
-  }
+  Widget build(BuildContext context) => DownloadProcessesDisplay();
 }
