a7806a8ddbd28356b4241f7d0e7428e0482ff115
nicholas-roether <nicholas.roether@t-online.de>
2020-11-01 08:31:17 +0100

    Add onTap callback

--- lib/screens/home/pages/videos.dart
+++ lib/screens/home/pages/videos.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:yt_snatcher/screens/home/downloads_display.dart';
+import 'package:yt_snatcher/screens/watch/watch_screen.dart';
 
 class Videos extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return DownloadsDisplay((dlm) => dlm.getVideos());
+    return DownloadsDisplay(
+      selector: (dlm) => dlm.getVideos(),
+      onTap: (dl) => Navigator.pushNamed(
+        context,
+        WatchScreen.ROUTENAME,
+        arguments: dl,
+      ),
+    );
   }
 }
