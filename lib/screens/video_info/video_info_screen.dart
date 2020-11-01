71d2c7ffdd65535f3de67e2e2dea37472c8bb24d
nicholas-roether <nicholas.roether@t-online.de>
2020-11-01 08:32:09 +0100

    Refactor yt_video_info to match file naming pattern

--- lib/screens/video_info/video_info_screen.dart
+++ lib/screens/video_info/video_info_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:yt_snatcher/services/youtube.dart';
 import 'package:yt_snatcher/widgets/screen.dart';
-import 'package:yt_snatcher/widgets/yt-video-info.dart';
+import 'package:yt_snatcher/widgets/yt_video_info.dart';
 
 class VideoInfoScreen extends StatelessWidget {
   static const ROUTENAME = "/videoInfo";
