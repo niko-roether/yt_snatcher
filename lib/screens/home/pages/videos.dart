07f691b779e5e63c8051d5487ded2708f6f7693d
nicholas-roether <nicholas.roether@t-online.de>
2020-11-01 09:19:06 +0100

    Remove onTap

--- lib/screens/home/pages/videos.dart
+++ lib/screens/home/pages/videos.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:yt_snatcher/screens/home/downloads_display.dart';
-import 'package:yt_snatcher/screens/watch/watch_screen.dart';
 
 class Videos extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return DownloadsDisplay(
-      selector: (dlm) => dlm.getVideos(),
-      onTap: (dl) => Navigator.pushNamed(
-        context,
-        WatchScreen.ROUTENAME,
-        arguments: dl,
-      ),
-    );
+    return DownloadsDisplay(selector: (dlm) => dlm.getVideos());
   }
 }
