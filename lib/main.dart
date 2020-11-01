86881fc74705727443a7883e47fbeac3f7ea51c6
nicholas-roether <nicholas.roether@t-online.de>
2020-11-01 13:01:13 +0100

    Use ServiceProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
 import "package:flutter/material.dart";
 import 'package:yt_snatcher/router.dart';
-import 'package:yt_snatcher/widgets/provider/download_process_manager.dart';
-import 'package:yt_snatcher/widgets/provider/download_provider.dart';
+import 'package:yt_snatcher/service_provider.dart';
+import 'package:yt_snatcher/themes/dark.dart';
 
 void main() => runApp(YtSnatcher());
 
 class YtSnatcher extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return DownloadProvider(
-      child: DownloadProcessManager(
-        child: MaterialApp(
-          title: "Youtube Snatcher",
-          initialRoute: "/",
-          routes: routes,
-        ),
+    return ServiceProvider(
+      child: MaterialApp(
+        title: "Youtube Snatcher",
+        initialRoute: "/",
+        routes: routes,
+        theme: darkTheme,
       ),
     );
   }
