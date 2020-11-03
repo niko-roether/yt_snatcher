cb4e4df3b4085d4afb6ef4a3e6fe08bc86a5571c
Ph4ntom3 <nicholas.roether@t-online.de>
2020-11-03 22:13:51 +0100

    Fix error provider

--- lib/service_provider.dart
+++ lib/service_provider.dart
@@ -1,44 +1,21 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:yt_snatcher/widgets/provider/download_process_manager.dart';
 import 'package:yt_snatcher/widgets/provider/download_provider.dart';
-import 'package:yt_snatcher/widgets/provider/error_stream_provider.dart';
+import 'package:yt_snatcher/widgets/provider/error_provider.dart';
 
-class ServiceProvider extends StatefulWidget {
+class ServiceProvider extends StatelessWidget {
   final Widget child;
 
   ServiceProvider({@required this.child});
 
   @override
-  State<StatefulWidget> createState() {
-    return ServiceProviderState();
-  }
-}
-
-class ServiceProviderState extends State<ServiceProvider> {
-  final _errorStreamController = StreamController<Object>();
-  Stream<Object> _errorStream;
-
-  ServiceProviderState() {
-    _errorStream = _errorStreamController.stream.asBroadcastStream();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return ErrorStreamProvider(
-        child: DownloadProvider(
-          child: DownloadProcessManager(
-            child: widget.child,
-          ),
+    return ErrorProviderManager(
+      child: DownloadProvider(
+        child: DownloadProcessManager(
+          child: child,
         ),
-        controller: _errorStreamController,
-        stream: _errorStream);
-  }
-
-  @override
-  void dispose() {
-    _errorStreamController.close();
-    super.dispose();
+      ),
+    );
   }
 }
---
+++ lib/widgets/provider/error_provider.dart
@@ -0,0 +1,57 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class ErrorProvider extends InheritedWidget {
+  final Stream<Object> stream;
+  final void Function(Object) add;
+
+  ErrorProvider({
+    Key key,
+    @required this.stream,
+    @required this.add,
+    Widget child,
+  }) : super(key: key, child: child);
+
+  factory ErrorProvider.of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<ErrorProvider>();
+  }
+
+  @override
+  bool updateShouldNotify(ErrorProvider old) => stream != old.stream;
+}
+
+class ErrorProviderManager extends StatefulWidget {
+  final Widget child;
+
+  ErrorProviderManager({this.child});
+
+  @override
+  State<StatefulWidget> createState() {
+    return ErrorProviderManagerState();
+  }
+}
+
+class ErrorProviderManagerState extends State<ErrorProviderManager> {
+  final _controller = StreamController<Object>();
+  Stream<Object> _stream;
+
+  ErrorProviderManagerState() {
+    _stream = _controller.stream.asBroadcastStream();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ErrorProvider(
+      stream: _stream,
+      add: (e) => _controller.add(e),
+      child: widget.child,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.close();
+    super.dispose();
+  }
+}
