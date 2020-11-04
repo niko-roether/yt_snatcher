e51f8f383944254f7ecb58afccf8a5e87763a326
nicholas-roether <nicholas.roether@t-online.de>
2020-11-04 13:36:22 +0100

    Reimplement DownloadPlayer

--- lib/widgets/download_player.dart
+++ lib/widgets/download_player.dart
@@ -1,54 +1,13 @@
+import 'dart:async';
+
 import 'package:better_player/better_player.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:yt_snatcher/screens/video_info/video_info_screen.dart';
 import 'package:yt_snatcher/services/download_manager.dart';
+import 'package:yt_snatcher/widgets/provider/orientation_provider.dart';
 
-// class DownloadPlayer extends StatelessWidget {
-//   final Download download;
-//   final bool screenSleep;
-//   final bool defaultFullscreen;
-
-//   DownloadPlayer({
-//     @required this.download,
-//     this.screenSleep = true,
-//     this.defaultFullscreen = false,
-//   });
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final controlsConfig = BetterPlayerControlsConfiguration(
-//       overflowMenuIconsColor: Theme.of(context).iconTheme.color,
-//       overflowMenuCustomItems: [
-//         BetterPlayerOverflowMenuItem(
-//           Icons.info,
-//           "Info",
-//           () => Navigator.pushNamed(
-//             context,
-//             VideoInfoScreen.ROUTENAME,
-//             arguments: download.meta.videoMeta,
-//           ),
-//         ),
-//       ],
-//     );
-//     return BetterPlayer.file(
-//       download.mediaFile.path,
-//       betterPlayerConfiguration: BetterPlayerConfiguration(
-//         allowedScreenSleep: screenSleep,
-//         fullScreenByDefault: defaultFullscreen,
-//         deviceOrientationsAfterFullScreen: [
-//           DeviceOrientation.landscapeLeft,
-//           DeviceOrientation.landscapeLeft,
-//           DeviceOrientation.portraitUp,
-//           DeviceOrientation.portraitDown,
-//         ],
-//         autoPlay: true,
-//         fit: BoxFit.contain,
-//         controlsConfiguration: controlsConfig,
-//       ),
-//     );
-//   }
-// }
+import 'consumer.dart';
 
 class DownloadPlayer extends StatefulWidget {
   final List<Download> downloads;
@@ -82,15 +41,60 @@ class DownloadPlayer extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
+    return DownloadPlayerState();
   }
 }
 
 class DownloadPlayerState extends State<DownloadPlayer> {
+  BetterPlayerController _controller;
+  List<BetterPlayerDataSource> _dataSources;
+  int _index;
+
+  void _playerEvent(BetterPlayerEventType type, Map<String, dynamic> args) {
+    if (type == BetterPlayerEventType.CHANGED_TRACK) {
+      print(args);
+    }
+  }
+
+  @override
+  void initState() {
+    _index = widget.startAt;
+    _dataSources = widget.downloads
+        .map((d) => BetterPlayerDataSource(
+            BetterPlayerDataSourceType.FILE, d.mediaFile.path))
+        .toList();
+    _controller = BetterPlayerController(
+        BetterPlayerConfiguration(
+          allowedScreenSleep: widget.screenSleep,
+          fullScreenByDefault: widget.defaultFullscreen,
+          fit: BoxFit.contain,
+          autoPlay: true,
+          controlsConfiguration: BetterPlayerControlsConfiguration(
+            overflowMenuIconsColor: Color(0xffffffff),
+            enableOverflowMenu: false,
+          ),
+          eventListener: (e) =>
+              _playerEvent(e.betterPlayerEventType, e.parameters),
+        ),
+        betterPlayerDataSource:
+            _dataSources[0] // TODO support mulitple data sources
+        );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Consumer<OrientationProvider>(
+      builder: (context, provider, child) {
+        bool fullscreen = provider.orientation == Orientation.landscape;
+        if (fullscreen) {
+          Timer.run(() => _controller.toggleFullScreen());
+        }
+        return child;
+      },
+      child: BetterPlayer(
+        controller: _controller,
+      ),
+    );
   }
 }
