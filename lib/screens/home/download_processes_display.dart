f1d370ff0c83e47f18310dedd9df38980cd47fab
nicholas-roether <nicholas.roether@t-online.de>
2020-11-04 16:49:07 +0100

    Use new OngoingDownloadView

--- lib/screens/home/download_processes_display.dart
+++ lib/screens/home/download_processes_display.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:yt_snatcher/widgets/consumer.dart';
-import 'package:yt_snatcher/widgets/media_downloader_view.dart';
+import 'package:yt_snatcher/widgets/ongoing_download_view.dart';
 import 'package:yt_snatcher/widgets/provider/download_process_manager.dart';
 
 class DownloadProcessesDisplay extends StatelessWidget {
@@ -18,9 +18,8 @@ class DownloadProcessesDisplay extends StatelessWidget {
         return ListView.builder(
           itemBuilder: (context, i) {
             var process = processes[i];
-            return MediaDownloaderView(
-              downloader: process.downloader,
-              meta: process.meta,
+            return OngoingDownloadView(
+              ongoingDownload: process,
             );
           },
           itemCount: processes.length,
