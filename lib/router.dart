bf6020f0a5ac59c07b9385e5ccbf57f40cf98f27
nicholas-roether <nicholas.roether@t-online.de>
2020-11-04 13:35:45 +0100

    Refactor PlayScreen to WatchScreen

--- lib/router.dart
+++ lib/router.dart
@@ -1,13 +1,13 @@
 import 'package:yt_snatcher/screens/download/download_screen.dart';
 import 'package:yt_snatcher/screens/home/home_screen.dart';
 import 'package:yt_snatcher/screens/video_info/video_info_screen.dart';
-import 'package:yt_snatcher/screens/play/play_screen.dart';
+import 'package:yt_snatcher/screens/watch/watch_screen.dart';
 import 'package:yt_snatcher/screens/settings/settings_screen.dart';
 
 var routes = {
   HomeScreen.ROUTENAME: (context) => HomeScreen(),
   VideoInfoScreen.ROUTENAME: (context) => VideoInfoScreen(),
   DownloadScreen.ROUTENAME: (context) => DownloadScreen(),
-  PlayScreen.ROUTENAME: (context) => PlayScreen(),
+  WatchScreen.ROUTENAME: (context) => WatchScreen(),
   SettingsScreen.ROUTENAME: (context) => SettingsScreen()
 };
--- lib/screens/home/downloads_display.dart
+++ lib/screens/home/downloads_display.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:yt_snatcher/screens/play/play_screen.dart';
+import 'package:yt_snatcher/screens/watch/watch_screen.dart';
 import 'package:yt_snatcher/services/download_manager.dart';
 import 'package:yt_snatcher/widgets/download_view.dart';
 import 'package:yt_snatcher/widgets/provider/download_provider.dart';
@@ -60,7 +60,7 @@ class DownloadsDisplayState extends State<DownloadsDisplay> {
             download: download,
             onTap: () => Navigator.pushNamed(
               context,
-              PlayScreen.ROUTENAME,
+              WatchScreen.ROUTENAME,
               arguments: download,
             ),
             onChange: () => _refresh(),
