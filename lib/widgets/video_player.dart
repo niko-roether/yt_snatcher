d8de30f0b14cb566381423372bf0df2570d429a4
Ph4ntom3 <nicholas.roether@t-online.de>
2020-11-05 16:05:45 +0100

    Create rudimentary video player

--- lib/widgets/video_player.dart
+++ lib/widgets/video_player.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_vlc_player/flutter_vlc_player.dart';
+
+enum VideoSourceType { FILE, NETWORK }
 
 class VideoPlayer extends StatefulWidget {
+  final String url;
+  final VideoSourceType type;
+
+  VideoPlayer({@required this.url, @required this.type}) : assert(type != null);
+
   @override
   State<StatefulWidget> createState() {
     return _VideoPlayerState();
@@ -8,9 +17,26 @@ class VideoPlayer extends StatefulWidget {
 }
 
 class _VideoPlayerState extends State<VideoPlayer> {
+  final _playerSize = Size(640, 360);
+  VlcPlayerController _controller;
+
+  _VideoPlayerState() {
+    _controller = VlcPlayerController(onInit: () {
+      _controller.play();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return SizedBox.fromSize(
+      size: _playerSize,
+      child: VlcPlayer(
+        aspectRatio: 16 / 9,
+        controller: _controller,
+        url: widget.url,
+        isLocalMedia: widget.type == VideoSourceType.FILE,
+        placeholder: Center(child: CircularProgressIndicator()),
+      ),
+    );
   }
 }
