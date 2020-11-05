ea63ad3bb89b0b76fc00bb121cb9d4e3de3b5ed7
Ph4ntom3 <nicholas.roether@t-online.de>
2020-11-05 16:06:10 +0100

    Use new video player

--- lib/screens/watch/download_player.dart
+++ lib/screens/watch/download_player.dart
@@ -1,11 +1,8 @@
-import 'dart:async';
-
-import 'package:better_player/better_player.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:yt_snatcher/services/download_manager.dart';
+import 'package:yt_snatcher/widgets/video_player.dart';
 
-class DownloadPlayer extends StatefulWidget {
+class DownloadPlayer extends StatelessWidget {
   final List<Download> downloads;
   final int startAt;
   final bool screenSleep;
@@ -36,70 +33,10 @@ class DownloadPlayer extends StatefulWidget {
   }
 
   @override
-  State<StatefulWidget> createState() {
-    return _DownloadPlayerState();
-  }
-}
-
-class _DownloadPlayerState extends State<DownloadPlayer> {
-  BetterPlayerController _controller;
-  List<BetterPlayerDataSource> _dataSources;
-  int _index;
-  bool _keepFullscreen = true;
-
-  void _playerEvent(BetterPlayerEventType type, Map<String, dynamic> args) {
-    if (type == BetterPlayerEventType.HIDE_FULLSCREEN) {
-      print(args);
-    }
-  }
-
-  @override
-  void initState() {
-    _index = widget.startAt;
-    _dataSources = widget.downloads
-        .map((d) => BetterPlayerDataSource(
-            BetterPlayerDataSourceType.FILE, d.mediaFile.path))
-        .toList();
-    _controller = BetterPlayerController(
-        BetterPlayerConfiguration(
-          allowedScreenSleep: widget.screenSleep,
-          fullScreenByDefault: widget.defaultFullscreen,
-          fit: BoxFit.contain,
-          autoPlay: true,
-          deviceOrientationsOnFullScreen: [
-            DeviceOrientation.landscapeLeft,
-            DeviceOrientation.landscapeRight,
-            DeviceOrientation.portraitUp,
-            DeviceOrientation.portraitDown,
-          ],
-          controlsConfiguration: BetterPlayerControlsConfiguration(
-            overflowMenuIconsColor: Color(0xffffffff),
-            enableOverflowMenu: false,
-          ),
-          eventListener: (e) =>
-              _playerEvent(e.betterPlayerEventType, e.parameters),
-        ),
-        betterPlayerDataSource:
-            _dataSources[0] // TODO support mulitple data sources
-        );
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final orientation = MediaQuery.of(context).orientation;
-    if (orientation == Orientation.landscape && !_controller.isFullScreen) {
-      _keepFullscreen = false;
-      Timer.run(() => _controller.enterFullScreen());
-    } else if (orientation == Orientation.portrait &&
-        _controller.isFullScreen) {
-      if (!_keepFullscreen) {
-        Timer.run(() => _controller.exitFullScreen());
-      }
-      _keepFullscreen = true;
-    }
-    return BetterPlayer(
-      controller: _controller,
+    return VideoPlayer(
+      url: downloads[0].mediaFile.path,
+      type: VideoSourceType.FILE,
     );
   }
 }
