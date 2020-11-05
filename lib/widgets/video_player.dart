88def86137ca8238faa3b026733188d3e9945553
Ph4ntom3 <nicholas.roether@t-online.de>
2020-11-05 15:10:40 +0100

    Create video player component

---
+++ lib/widgets/video_player.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class VideoPlayer extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _VideoPlayerState();
+  }
+}
+
+class _VideoPlayerState extends State<VideoPlayer> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
