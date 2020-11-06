c74be8abde464e7612b93772b08e88c9cb7db855
Ph4ntom3 <nicholas.roether@t-online.de>
2020-11-06 17:52:15 +0100

    Add fade animation

--- lib/widgets/video_player/video_player_controls.dart
+++ lib/widgets/video_player/video_player_controls.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_vlc_player/flutter_vlc_player.dart';
@@ -18,35 +20,166 @@ class VideoPlayerControls extends StatefulWidget {
   State<StatefulWidget> createState() => VideoPlayerControlsState();
 }
 
-class VideoPlayerControlsState extends State<VideoPlayerControls> {
-  bool show;
+class VideoPlayerControlsState extends State<VideoPlayerControls>
+    with SingleTickerProviderStateMixin {
+  static const _HIDE_DURATION = Duration(seconds: 3);
+  AnimationController _showHideAnimation;
+  bool _shown = false;
+  bool _playing = false;
+  Timer _hideTimer;
 
   @override
   void initState() {
-    show = widget.showControlsImmediately;
+    _showHideAnimation = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 100),
+    );
+    widget.showControlsImmediately ? _show() : _hide();
+    widget.controller.addListener(_onControllerUpdate);
     super.initState();
   }
 
   @override
+  void dispose() {
+    _hideTimer?.cancel();
+    widget.controller.removeListener(_onControllerUpdate);
+    super.dispose();
+  }
+
+  void _onControllerUpdate() async {
+    final playing = await widget.controller.isPlaying();
+    if (!this.mounted) return;
+    if (playing != _playing) {
+      _playing = playing;
+      if (_playing) _scheduleHide();
+    }
+  }
+
+  void _show() {
+    _hideTimer?.cancel();
+    _shown = true;
+    if (_playing) _scheduleHide();
+  }
+
+  void _hide() {
+    _hideTimer?.cancel();
+    _shown = false;
+  }
+
+  void _scheduleHide() {
+    _hideTimer = Timer(_HIDE_DURATION, () {
+      if (_playing) setState(() => _hide());
+    });
+  }
+
+  void _onTap() {
+    setState(() => _shown ? _hide() : _show());
+  }
+
+  @override
   Widget build(BuildContext context) {
+    _showHideAnimation.animateTo(_shown ? 1 : 0);
     return GestureDetector(
       behavior: HitTestBehavior.translucent,
-      onTap: () => setState(() => show = !show),
+      onTap: () => _onTap(),
       child: AspectRatio(
         aspectRatio: widget.aspectRatio,
-        child: Column(
-          children: [
-            Container(),
-            Container(),
-            _VideoPlayerControlsBottomBar(
-              controller: widget.controller,
-              expanded: show,
-            ),
-          ],
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: AnimatedBuilder(
+          animation: _showHideAnimation,
+          builder: (context, child) {
+            return Container(
+              color: Colors.black26.withAlpha(
+                (_showHideAnimation.value * 70).round(),
+              ),
+              child: child,
+            );
+          },
+          child: Stack(
+            children: [
+              Container(),
+              Align(
+                child: _VideoPlayerControlsCenter(
+                  controller: widget.controller,
+                  visible: _shown,
+                ),
+                alignment: Alignment.center,
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: _VideoPlayerControlsBottomBar(
+                  controller: widget.controller,
+                  expanded: _shown,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+class _VideoPlayerControlsCenter extends StatefulWidget {
+  final VlcPlayerController controller;
+  final bool visible;
+
+  _VideoPlayerControlsCenter({@required this.controller, this.visible = true})
+      : assert(visible != null);
+
+  @override
+  State<StatefulWidget> createState() => _VideoPlayerControlsCenterState();
+}
+
+class _VideoPlayerControlsCenterState
+    extends State<_VideoPlayerControlsCenter> {
+  VlcPlayerController _controller;
+  PlayingState _state;
+
+  @override
+  void initState() {
+    _controller = widget.controller;
+    _state = _controller.playingState ?? PlayingState.PAUSED;
+    _controller.addListener(_onControllerUpdate);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(_onControllerUpdate);
+    super.dispose();
+  }
+
+  void _onControllerUpdate() {
+    final newState = _controller.playingState;
+    if (newState != _state) setState(() => _state = newState);
+  }
+
+  Widget _buildPlayButton() {
+    return IconButton(
+      icon: Icon(Icons.play_arrow),
+      onPressed: () => _controller.play(),
+      iconSize: 40,
+    );
+  }
+
+  Widget _buildPauseButton() {
+    return IconButton(
+      icon: Icon(Icons.pause),
+      onPressed: () => _controller.pause(),
+      iconSize: 40,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!widget.visible) return Container();
+    var center;
+    if ([PlayingState.PLAYING, PlayingState.BUFFERING].contains(_state))
+      center = _buildPauseButton();
+    else
+      center = _buildPlayButton();
+
+    return center;
   }
 }
 
@@ -73,18 +206,19 @@ class _VideoPlayerControlsBottomBarState
   @override
   void initState() {
     _controller = widget.controller;
-    _controller.addListener(() => _onControllerUpdate());
-    _position = Duration.zero;
+    _controller.addListener(_onControllerUpdate);
+    _position = _controller.position ?? Duration.zero;
     super.initState();
   }
 
   @override
   dispose() {
-    _controller.removeListener(() => _onControllerUpdate());
+    _controller.removeListener(_onControllerUpdate);
     super.dispose();
   }
 
   double _getProgress() {
+    if (_controller.duration == Duration.zero) return 0;
     return _position.inMilliseconds / _controller.duration.inMilliseconds;
   }
 
