5db8e52679b331903cd15e5d3cf4283c446ae4b7
Ph4ntom3 <nicholas.roether@t-online.de>
2020-11-06 18:21:09 +0100

    Add animation

--- lib/widgets/video_player/video_progress_bar.dart
+++ lib/widgets/video_player/video_progress_bar.dart
@@ -1,62 +1,100 @@
 import 'package:flutter/material.dart';
 
-class VideoProgressBar extends StatelessWidget {
-  static const double _NORMAL_HEIGHT = 2;
-  static const double _DRAGGABLE_HEIGHT = 5;
+class VideoProgressBar extends StatefulWidget {
   final double progress;
   final bool draggable;
+  final Duration animationDuration;
 
-  VideoProgressBar({this.progress, this.draggable = false})
-      : assert(draggable != null);
+  VideoProgressBar({
+    this.progress,
+    this.draggable = false,
+    this.animationDuration = const Duration(milliseconds: 100),
+  }) : assert(draggable != null);
+
+  @override
+  State<StatefulWidget> createState() => _VideoProgressBarState();
+}
+
+class _VideoProgressBarState extends State<VideoProgressBar>
+    with SingleTickerProviderStateMixin {
+  static const double _NORMAL_HEIGHT = 2;
+  static const double _DRAGGABLE_HEIGHT = 5;
+  AnimationController _animationController;
+
+  @override
+  initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: widget.animationDuration,
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
+    if (widget.draggable)
+      _animationController.animateTo(1);
+    else
+      _animationController.animateTo(0);
     return CustomPaint(
       painter: _VideoProgressBarPainter(
-        progress,
-        draggable,
-        Theme.of(context).colorScheme.primary,
+        progress: widget.progress,
+        color: Theme.of(context).colorScheme.primary,
+        normalBarWidth: _NORMAL_HEIGHT,
+        draggableBarWidth: _DRAGGABLE_HEIGHT,
+        expand: _animationController,
       ),
       size: Size(
         width,
-        draggable ? _DRAGGABLE_HEIGHT : _NORMAL_HEIGHT,
+        widget.draggable ? _DRAGGABLE_HEIGHT : _NORMAL_HEIGHT,
       ),
     );
   }
 }
 
 class _VideoProgressBarPainter extends CustomPainter {
-  static const double _NORMAL_BAR_WIDTH = VideoProgressBar._NORMAL_HEIGHT;
-  static const double _DRAGGABLE_BAR_WIDTH = VideoProgressBar._DRAGGABLE_HEIGHT;
-  final double _progress;
-  final bool _draggable;
-  final Color _color;
+  static const double _CIRCLE_SIZE_FACTOR = 1.2;
+  final double progress;
+  final Color color;
+  final double normalBarWidth;
+  final double draggableBarWidth;
+  final double circleRadius;
+  final Animation<double> expand;
 
-  _VideoProgressBarPainter(this._progress, this._draggable, this._color);
+  _VideoProgressBarPainter({
+    this.expand = const AlwaysStoppedAnimation(0),
+    this.progress,
+    this.color,
+    this.normalBarWidth = 2,
+    this.draggableBarWidth = 5,
+  }) : circleRadius = draggableBarWidth * _CIRCLE_SIZE_FACTOR;
 
   @override
   void paint(Canvas canvas, Size size) {
+    final double strokeWidth =
+        (draggableBarWidth - normalBarWidth) * expand.value + normalBarWidth;
     final paint = Paint()
       ..style = PaintingStyle.stroke
-      ..strokeWidth = _draggable ? _DRAGGABLE_BAR_WIDTH : _NORMAL_BAR_WIDTH;
-    paint.color = _color;
-    final lineStart = Offset(0, size.height / 2);
-    final lineEnd = Offset((_progress ?? 0) * size.width, size.height / 2);
+      ..strokeWidth = strokeWidth;
+
+    paint.color = color;
+    final lineStart = Offset(0, strokeWidth / 2);
+    final lineEnd = Offset((progress ?? 0) * size.width, strokeWidth / 2);
     canvas.drawLine(
       lineStart,
       lineEnd,
       paint,
     );
-    if (!_draggable) return;
+    if (expand.value == 0) return;
     final circlePaint = Paint()
       ..style = PaintingStyle.fill
-      ..color = _color;
-    canvas.drawCircle(lineEnd, _DRAGGABLE_BAR_WIDTH * 1.2, circlePaint);
+      ..color = color;
+    canvas.drawCircle(lineEnd, circleRadius * expand.value, circlePaint);
   }
 
   @override
   bool shouldRepaint(_VideoProgressBarPainter old) {
-    return _progress != old._progress || _draggable != old._draggable;
+    return progress != old.progress || expand.value != old.expand.value;
   }
 }
