25e6f7252f09d96284d97cb3a1b44f3f73def0f5
nicholas-roether <nicholas.roether@t-online.de>
2020-11-07 18:53:26 +0100

    Adjust better to environment

--- lib/widgets/video_player/video_player_controls_top.dart
+++ lib/widgets/video_player/video_player_controls_top.dart
@@ -4,20 +4,54 @@ import 'package:flutter/material.dart';
 class VideoPlayerControlsTop extends StatelessWidget {
   final void Function() onBack;
   final bool visible;
+  final bool fullscreen;
+  final Animation<double> animation;
 
-  VideoPlayerControlsTop({this.visible = true, this.onBack});
+  VideoPlayerControlsTop({
+    this.visible = true,
+    this.fullscreen = false,
+    this.onBack,
+    @required this.animation,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () => onBack?.call(),
-          visualDensity: VisualDensity.compact,
-          padding: EdgeInsets.zero,
-        ),
-      ],
+    final button = IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () => onBack?.call(),
+      visualDensity: VisualDensity.compact,
+    );
+    Widget widget;
+    if (fullscreen) {
+      widget = visible
+          ? Align(
+              child: button,
+              alignment: Alignment.topLeft,
+            )
+          : Container();
+    } else {
+      widget = Row(
+        children: [
+          AnimatedBuilder(
+            animation: fullscreen ? AlwaysStoppedAnimation(0) : animation,
+            builder: (context, child) {
+              return DecoratedBox(
+                decoration: ShapeDecoration(
+                  color:
+                      Colors.black.withAlpha((animation.value * 100).floor()),
+                  shape: CircleBorder(),
+                ),
+                child: child,
+              );
+            },
+            child: button,
+          ),
+        ],
+      );
+    }
+    return Padding(
+      padding: EdgeInsets.all(8).copyWith(bottom: 0),
+      child: widget,
     );
   }
 }
