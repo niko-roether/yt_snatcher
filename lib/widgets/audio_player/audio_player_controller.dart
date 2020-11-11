a6bb71298f7615fd3ce13e1cd54d804ed8cba91b
Ph4ntom3 <nicholas.roether@t-online.de>
2020-11-11 21:20:16 +0100

    Implement some audio controllery things

--- lib/widgets/audio_player/audio_player_controller.dart
+++ lib/widgets/audio_player/audio_player_controller.dart
@@ -1,55 +1,156 @@
+import 'dart:async';
+
 import 'package:audio_service/audio_service.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:audio_session/audio_session.dart';
 import 'package:just_audio/just_audio.dart';
-import 'package:yt_snatcher/services/download_manager.dart';
 
-class _AudioPlayTask extends BackgroundAudioTask {
-  final AudioPlayer _player = AudioPlayer();
-  final Download _download;
-  bool _initialized = false;
+class _AudioPlayerTask extends BackgroundAudioTask {
+  final _player = AudioPlayer();
+  List<MediaItem> _queue = [];
+  Timer _seeker;
+  StreamSubscription<PlaybackEvent> _playerEventSubscription;
+  AudioProcessingState _stateOverride;
 
-  _AudioPlayTask(this._download)
-      : assert(_download.meta.type == DownloadType.MUSIC);
+  Future<void> _seekRelative(Duration amount) {
+    var pos = _player.duration + amount;
+    if (pos < Duration.zero) pos = Duration.zero;
+    if (pos > _player.duration) pos = _player.duration;
+    return _player.seek(pos);
+  }
 
-  Future<void> _init() async {
-    _initialized = true;
-    await _player.load(DashAudioSource(_download.mediaFile.uri));
+  void _seekContinuous(bool begin, int direction) {
+    _seeker?.cancel();
+    if (!begin) return;
+    _seeker = Timer.periodic(Duration(seconds: 1), (_) {
+      _seekRelative(Duration(seconds: 10 * direction));
+    });
+  }
+
+  AudioProcessingState get _processingState {
+    if (_stateOverride != null) return _stateOverride;
+    switch (_player.processingState) {
+      case ProcessingState.none:
+        return AudioProcessingState.stopped;
+      case ProcessingState.loading:
+        return AudioProcessingState.connecting;
+      case ProcessingState.buffering:
+        return AudioProcessingState.buffering;
+      case ProcessingState.ready:
+        return AudioProcessingState.ready;
+      case ProcessingState.completed:
+        return AudioProcessingState.completed;
+      default:
+        throw Exception("${_player.processingState} is not a valid state");
+    }
+  }
+
+  Future<void> _onStateChange() {
+    return AudioServiceBackground.setState(
+      controls: [
+        if (_player.hasPrevious) MediaControl.skipToPrevious,
+        _player.playing ? MediaControl.pause : MediaControl.play,
+        MediaControl.pause,
+        if (_player.hasNext) MediaControl.skipToNext,
+      ],
+      systemActions: [
+        MediaAction.seekTo,
+        MediaAction.seekForward,
+        MediaAction.seekBackward,
+      ],
+      androidCompactActions: [0, 1, 3],
+      processingState: _processingState,
+      playing: _player.playing,
+      position: _player.position,
+      bufferedPosition: _player.bufferedPosition,
+      speed: _player.speed,
+    );
   }
 
   @override
-  Future<void> onPlay() async {
-    if (!_initialized) await _init();
-    await _player.play();
-    await AudioServiceBackground.setState(
-      controls: [MediaControl.pause],
-      processingState: AudioProcessingState.completed,
-      playing: true,
-    );
-    await AudioServiceBackground.setMediaItem(MediaItem(
-      id: _download.meta.id,
-      album: _download.meta.videoMeta.channelName,
-      title: _download.meta.displayTitle,
-    ));
-  }
+  Future<void> onStart(Map<String, dynamic> params) async {
+    final session = await AudioSession.instance;
+    session.configure(AudioSessionConfiguration.music());
 
-  @override
-  Future<void> onPause() async {
-    await _player.play();
-    await AudioServiceBackground.setState(
-      controls: [MediaControl.play],
-      processingState: AudioProcessingState.completed,
-      playing: false,
-    );
+    _player.currentIndexStream.listen((i) {
+      if (i != null) AudioServiceBackground.setMediaItem(_queue[i]);
+    });
+    _playerEventSubscription = _player.playbackEventStream.listen((_) {
+      _onStateChange();
+    });
+    _player.processingStateStream.listen((state) {
+      switch (state) {
+        case ProcessingState.completed:
+          onStop();
+          break;
+        case ProcessingState.ready:
+          _stateOverride = null;
+          break;
+        default:
+      }
+    });
+
+    AudioServiceBackground.setQueue(_queue);
+    try {
+      await _player.load(ConcatenatingAudioSource(
+        children:
+            _queue.map((item) => AudioSource.uri(Uri.file(item.id))).toList(),
+      ));
+      onPlay();
+    } catch (e) {
+      print("Error: $e");
+      onStop();
+    }
   }
 
   @override
   Future<void> onStop() async {
-    await AudioServiceBackground.setMediaItem(MediaItem(
-      id: null,
-      album: null,
-      title: null,
-    ));
+    await _playerEventSubscription.cancel();
+    await _player.pause();
+    await _player.dispose();
+    await _onStateChange();
+    return super.onStop();
   }
+
+  @override
+  Future<void> onAddQueueItem(MediaItem item) async => _queue.add(item);
+
+  @override
+  Future<void> onAddQueueItemAt(MediaItem mediaItem, int index) async {
+    _queue.insert(index, mediaItem);
+  }
+
+  @override
+  Future<void> onSkipToQueueItem(String mediaId) async {
+    final index = _queue.indexWhere((item) => item.id == mediaId);
+    if (index == -1) return;
+    _stateOverride = index > _player.currentIndex
+        ? AudioProcessingState.skippingToNext
+        : AudioProcessingState.skippingToPrevious;
+    _player.seek(Duration.zero, index: index);
+  }
+
+  @override
+  Future<void> onPlay() => _player.play();
+
+  @override
+  Future<void> onPause() => _player.pause();
+
+  @override
+  Future<void> onSeekForward(bool begin) async => _seekContinuous(begin, 1);
+
+  @override
+  Future<void> onSeekBackward(bool begin) async => _seekContinuous(begin, -1);
+
+  @override
+  Future<void> onSeekTo(Duration position) => _player.seek(position);
+
+  @override
+  Future<void> onFastForward() => _seekRelative(fastForwardInterval);
+
+  @override
+  Future<void> onRewind() => _seekRelative(rewindInterval);
 }
 
-class AudioPlayerController extends ChangeNotifier {}
+void audioTaskEntryPoint() async {
+  AudioServiceBackground.run(() => _AudioPlayerTask());
+}
