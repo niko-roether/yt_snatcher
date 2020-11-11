1e9dc32ef159035f9572e1074a668e37aa49de02
Ph4ntom3 <nicholas.roether@t-online.de>
2020-11-11 21:21:09 +0100

    Add temporary listen button

--- lib/screens/home/pages/home.dart
+++ lib/screens/home/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yt_snatcher/screens/listen/listen_screen.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -6,9 +7,18 @@ class Home extends StatelessWidget {
     return Center(
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 16),
-        child: Text(
-          "Absolutely amazing home screen.\n\nBy using our app you agree to our privacy policy as well as to be absolutely blown away by how amazing our home screen is.",
-          textAlign: TextAlign.center,
+        child: Column(
+          children: [
+            Text(
+              "Absolutely amazing home screen.\n\nBy using our app you agree to our privacy policy as well as to be absolutely blown away by how amazing our home screen is.",
+              textAlign: TextAlign.center,
+            ),
+            TextButton(
+              onPressed: () =>
+                  Navigator.pushNamed(context, ListenScreen.ROUTENAME),
+              child: Text("listen"),
+            ),
+          ],
         ),
       ),
     );
